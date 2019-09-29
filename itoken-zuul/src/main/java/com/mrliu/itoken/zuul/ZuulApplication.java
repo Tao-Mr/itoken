2dfb5923c785ea115952ac33ba33952bb2abfc7d
Tao-Mr <30943700+Tao-Mr@users.noreply.github.com>
2019-09-29 16:06:51 +0800

    分布式路由网关

---
+++ itoken-zuul/src/main/java/com/mrliu/itoken/zuul/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.mrliu.itoken.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @ProjectName: itoken
+ * @Package: com.mrliu.itoken.zuul
+ * @Description: 分布式路由网关
+ * @Date: 2019/4/17 15:40
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
